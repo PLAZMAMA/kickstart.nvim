3634c575734780492dc588e9a30671174298114f
PLAZMAMA <maorioriori@gmail.com>
2024-03-04 15:19:16 -0500

    remove codeium for now to practice with Kinesis keyboard

--- lua/plazma/plugins/codeium.lua
+++ lua/plazma/plugins/codeium.lua
@@ -1,18 +1,20 @@
-return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter',
-  config = function()
-    vim.g.codeium_disable_bindings = 1
-    vim.keymap.set('i', '<C-a>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-    vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-    vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-    vim.keymap.set('i', '<M-c>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-    vim.keymap.set('n', '<leader>ma', function() vim.g.codeium_enabled = true end, { expr = true, silent = true })
-    vim.keymap.set('n', '<leader>md', function() vim.g.codeium_enabled = false end, { expr = true, silent = true })
-
-    require('which-key').register {
-      ['<leader>ma'] = { name = '[M]atrix(codeium) [A]ctivate', _ = 'which_key_ignore' },
-      ['<leader>md'] = { name = '[M]atrix(codeium) [D]eactivate', _ = 'which_key_ignore' },
-    }
- end,
-}
+return {}
+-- Commenting codium out for now to get practice using the kinesis
+-- return {
+--   'Exafunction/codeium.vim',
+--   event = 'BufEnter',
+--   config = function()
+--     vim.g.codeium_disable_bindings = 1
+--     vim.keymap.set('i', '<C-a>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+--     vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+--     vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+--     vim.keymap.set('i', '<M-c>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+--     vim.keymap.set('n', '<leader>ma', function() vim.g.codeium_enabled = true end, { expr = true, silent = true })
+--     vim.keymap.set('n', '<leader>md', function() vim.g.codeium_enabled = false end, { expr = true, silent = true })
+--
+--     require('which-key').register {
+--       ['<leader>ma'] = { name = '[M]atrix(codeium) [A]ctivate', _ = 'which_key_ignore' },
+--       ['<leader>md'] = { name = '[M]atrix(codeium) [D]eactivate', _ = 'which_key_ignore' },
+--     }
+--  end,
+-- }
